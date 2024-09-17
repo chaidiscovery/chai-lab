bf90332dddd57398cd183d4e4d2b9de817efd534
Kevin Wu <crazyviolindude@gmail.com>
2024-09-17 13:27:56 -0700

    Fix for cases where tokenization failed (#54)
    * Append None to structure_contexts on exception
    
    * Assign chain_id based on non-null contexts
    
    * Add test case
    
    * Apply suggestions from code review
    
    Co-authored-by: Alex Rogozhnikov <arogozhnikov@users.noreply.github.com>
    
    * Add comment
    
    ---------
    
    Co-authored-by: Alex Rogozhnikov <arogozhnikov@users.noreply.github.com>

---
+++ tests/test_inference_dataset.py
@@ -0,0 +1,33 @@
+"""
+Tests for inference dataset.
+"""
+
+from chai_lab.data.dataset.inference_dataset import Input, load_chains_from_raw
+from chai_lab.data.dataset.structure.all_atom_residue_tokenizer import (
+    AllAtomResidueTokenizer,
+)
+from chai_lab.data.parsing.structure.entity_type import EntityType
+from chai_lab.data.sources.rdkit import RefConformerGenerator
+
+
+def test_malformed_smiles():
+    """Malformed SMILES should be dropped."""
+    # Zn ligand is malformed (should be [Zn+2])
+    inputs = [
+        Input("RKDESES", entity_type=EntityType.PROTEIN.value, entity_name="foo"),
+        Input("Zn", entity_type=EntityType.LIGAND.value, entity_name="bar"),
+        Input("RKEEE", entity_type=EntityType.PROTEIN.value, entity_name="baz"),
+        Input("EEEEEEEEEEEE", entity_type=EntityType.PROTEIN.value, entity_name="boz"),
+    ]
+    chains = load_chains_from_raw(
+        inputs,
+        identifier="test",
+        tokenizer=AllAtomResidueTokenizer(RefConformerGenerator()),
+    )
+    assert len(chains) == 3
+    for chain in chains:
+        # NOTE this check is only valid because there are no residues that are tokenized per-atom
+        # Ensures that the entity data and the structure context in each chain are paired correctly
+        assert chain.structure_context.num_tokens == len(
+            chain.entity_data.full_sequence
+        )

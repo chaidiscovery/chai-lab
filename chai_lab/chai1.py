1c9ddb35f1fbbba3f106272ab5a155bfb7010445
Alex Rogozhnikov <arogozhnikov@users.noreply.github.com>
2024-09-20 11:35:22 -0400

    add pre-commit to verify license headers in python files (#68)
    * commit license header
    
    * add pre-commit to verify license headers in python files
    
    * add empty line after license header

--- chai_lab/chai1.py
+++ chai_lab/chai1.py
@@ -1,4 +1,8 @@
-# %%
+# Copyright (c) 2024 Chai Discovery, Inc.
+# This source code is licensed under the Chai Discovery Community License
+# Agreement (LICENSE.md) found in the root directory of this source tree.
+
+
 import math
 from collections import Counter
 from dataclasses import dataclass
---
+++ chai_lab/data/parsing/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2024 Chai Discovery, Inc.
+# This source code is licensed under the Chai Discovery Community License
+# Agreement (LICENSE.md) found in the root directory of this source tree.
+

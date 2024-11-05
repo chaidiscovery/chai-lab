716383479ec8207d4b1a1af2fd0db2831371019c
Alex Rogozhnikov <iamfullofspam@gmail.com>
2024-11-05 01:05:45 +0000

    add 1536 to AVAILABLE_MODEL_SIZES

--- chai_lab/data/collate/utils.py
+++ chai_lab/data/collate/utils.py
@@ -10,7 +10,7 @@ from chai_lab.data.dataset.structure.all_atom_structure_context import (
 
 # static graph is exported for different n_tokens,
 #  we pad to the closest one
-AVAILABLE_MODEL_SIZES = [256, 384, 512, 768, 1024, 2048]
+AVAILABLE_MODEL_SIZES = [256, 384, 512, 768, 1024, 1536, 2048]
 
 
 @dataclass(frozen=True)

21910a2a20dd9ab8b64449155e13a2cad13bd6f2
Kevin Wu <kevin@chaidiscovery.com>
2024-12-06 06:07:40 +0000

    Addtl glycan fixes

--- tests/test_glycans.py
+++ tests/test_glycans.py
@@ -5,7 +5,7 @@ from chai_lab.data.parsing.glycans import _glycan_string_to_sugars_and_bonds
 
 
 def test_complex_parsing():
-    glycan = "MAN(6-1 FUC)(4-1 MAN(6-1 MAN(6-1 MAN)))"
+    glycan = "MAN(6-1 FUC)(4-1 MAN(6-1 MAN(6-1 MAN)))".replace(" ", "")
     sugars, bonds = _glycan_string_to_sugars_and_bonds(glycan)
     assert len(sugars) == 5
 
@@ -23,8 +23,8 @@ def test_complex_parsing():
 
 
 def test_complex_parsing_2():
-    glycan = (
-        "MAN(4-1 FUC(4-1 MAN)(6-1 FUC(4-1 MAN)))(6-1 MAN(6-1 MAN(4-1 MAN)(6-1 FUC)))"
+    glycan = "MAN(4-1 FUC(4-1 MAN)(6-1 FUC(4-1 MAN)))(6-1 MAN(6-1 MAN(4-1 MAN)(6-1 FUC)))".replace(
+        " ", ""
     )
     sugars, bonds = _glycan_string_to_sugars_and_bonds(glycan)
     assert len(sugars) == 9

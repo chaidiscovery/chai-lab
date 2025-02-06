e78a40be9ed0d4f9acec8564482f5abf7c54ad5e
Kevin Wu <kevin@chaidiscovery.com>
2025-02-06 23:03:32 +0000

    Add some helper functions

---
+++ chai_lab/data/io/rcsb.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2024 Chai Discovery, Inc.
+# Licensed under the Apache License, Version 2.0.
+# See the LICENSE file for details.
+import urllib.request
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+
+def download_cif_file(pdb_id: str, directory: Path) -> Path:
+    """Download the cif file for the given PDB ID from RCSB into the directory."""
+    outfile = directory / f"{pdb_id}.cif.gz"
+    source_url = f"https://files.rcsb.org/download/{pdb_id}.cif.gz"
+    retrieved, _ = urllib.request.urlretrieve(url=source_url, filename=outfile)
+    retrieved_path = Path(retrieved)
+    assert retrieved_path.exists() and retrieved_path.stat().st_size > 0
+    return retrieved_path
+
+
+if __name__ == "__main__":
+    with TemporaryDirectory() as p:
+        download_cif_file("7FBI", Path(p))

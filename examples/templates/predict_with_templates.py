e1ce0930dc16cec3f1a7f8e973dce4d15e1cd772
Kevin Wu <kevin@chaidiscovery.com>
2025-02-07 22:10:12 +0000

    Enable MSA for templates example

--- examples/templates/predict_with_templates.py
+++ examples/templates/predict_with_templates.py
@@ -1,6 +1,9 @@
+import logging
 from pathlib import Path
 
 from chai_lab.chai1 import run_inference
+
+logging.basicConfig(level=logging.INFO)
 
 # See RCSB 7WCU
 example_fasta = """
@@ -20,6 +23,7 @@ output_dir = Path("/tmp/outputs")
 candidates = run_inference(
     fasta_file=fasta_path,
     output_dir=output_dir,
+    use_msa_server=True,
     template_hits_path=Path(__file__).parent / "pdb70.m8",
     seed=1234,
     device="cuda:0",

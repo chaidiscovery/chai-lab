f357ec24ed0a6097e500e49c82042a5e4ca0bd90
Kevin Wu <kevin@chaidiscovery.com>
2025-02-08 00:56:11 +0000

    Fixes

--- chai_lab/data/io/rcsb.py
+++ chai_lab/data/io/rcsb.py
@@ -14,10 +14,12 @@ def download_cif_file(pdb_id: str, directory: Path) -> Path:
         logging.warning(
             f"Destination for {pdb_id=} already exists: {outfile}; will not overwrite"
         )
+        return outfile
     source_url = f"https://files.rcsb.org/download/{pdb_id}.cif.gz"
     logging.info(f"Fetching {source_url} -> {outfile}")
     retrieved, _ = urllib.request.urlretrieve(url=source_url, filename=outfile)
     retrieved_path = Path(retrieved)
+    assert retrieved_path == outfile
     assert retrieved_path.exists() and retrieved_path.stat().st_size > 0
     return retrieved_path
 

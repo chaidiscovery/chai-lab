e2ae929c81b03535d0dc457c986371e8ce283726
Kevin Wu <kevin@chaidiscovery.com>
2025-02-15 01:59:52 +0000

    Implement reordering

--- chai_lab/data/dataset/msas/utils.py
+++ chai_lab/data/dataset/msas/utils.py
@@ -5,16 +5,17 @@
 import torch
 from einops import rearrange, reduce, repeat
 from torch import Tensor
+from torch.nn import functional as F
 
-from chai_lab.utils.typing import Bool, typecheck
+from chai_lab.utils.typing import Bool, Float, typecheck
 
 
 @typecheck
-def subsample_msa_rows(
+def _subsample_msa_rows(
     mask: Bool[Tensor, "1 depth tokens"],
     select_n_rows: int = 4096,
     generator: torch.Generator | None = None,
-) -> Bool[Tensor, "1 depth tokens"]:
+) -> Bool[Tensor, "depth"]:
     """Adjust masking to look at a random subset of msas.
 
     Returns input mask as-is if select_n_rows <= 0 or depth < select_n_rows."""
@@ -39,8 +40,50 @@ def subsample_msa_rows(
     # We should never sample empty MSA rows
     assert not (~nonnull_rows_mask[selected_row_indices]).any()
 
-    # Create a mask for selected row indices
     selection_mask = torch.zeros_like(nonnull_rows_mask)
     selection_mask[selected_row_indices] = True
+    return selection_mask
 
+
+@typecheck
+def subsample_and_remask_msa_mask(
+    mask: Bool[Tensor, "1 depth tokens"],
+    select_n_rows: int = 4096,
+    generator: torch.Generator | None = None,
+) -> Bool[Tensor, "1 depth tokens"]:
+    """Subsets the given mask by selecting random rows."""
+    # Create a mask for selected row indices
+    selection_mask = _subsample_msa_rows(
+        mask, select_n_rows=select_n_rows, generator=generator
+    )
     return mask & repeat(selection_mask, "d -> 1 d 1")
+
+
+@typecheck
+def subsample_and_reorder_msa_feats_n_mask(
+    feats: Float[Tensor, "1 depth tokens dim"],
+    mask: Bool[Tensor, "1 depth tokens"],
+    select_n_rows: int = 4096,
+    generator: torch.Generator | None = None,
+) -> tuple[Float[Tensor, "1 depth tokens dim"], Bool[Tensor, "1 depth tokens"]]:
+    selection_mask = _subsample_msa_rows(
+        mask=mask,
+        select_n_rows=select_n_rows,
+        generator=generator,
+    )
+
+    # Select the rows
+    (selection_idx,) = torch.where(selection_mask)
+    feats_sampled = torch.index_select(feats, dim=1, index=selection_idx)
+    mask_sampled = torch.index_select(mask, dim=1, index=selection_idx)
+
+    # Pad with zeros
+    _, orig_depth, _ = mask.shape
+    _, new_depth, _ = mask_sampled.shape
+    assert (n_pad := orig_depth - new_depth) > 0
+    # Padding is last dim, moving forward, e.g., for last two dimensions, it is:
+    # (left, right, top, bottom)
+    return (
+        F.pad(feats_sampled, pad=[0, 0, 0, 0, 0, n_pad], value=0.0),
+        F.pad(mask_sampled, pad=[0, 0, 0, n_pad], value=False),
+    )

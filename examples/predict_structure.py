3c68320cf0334922f00ef0cb6548d88770264df4
Alex Rogozhnikov <arogozhnikov@users.noreply.github.com>
2025-03-17 18:14:42 -0700

    do not use esm from transformers (#343)
    * do not use esm from transformers
    
    * minor

--- examples/predict_structure.py
+++ examples/predict_structure.py
@@ -6,6 +6,8 @@ import numpy as np
 
 from chai_lab.chai1 import run_inference
 
+logging.basicConfig(level=logging.INFO)  # control verbosity
+
 # We use fasta-like format for inputs.
 # - each entity encodes protein, ligand, RNA or DNA
 # - each entity is labeled with unique name;

260b48650fac7eed0a90f7dcff2b49128267e805
Kevin Wu <kevin@chaidiscovery.com>
2025-03-27 17:09:49 +0000

    Additional test

--- tests/test_colabfold_msas.py
+++ tests/test_colabfold_msas.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 
 from chai_lab.data.dataset.msas.colabfold import generate_colabfold_msas
+from chai_lab.data.dataset.msas.msa_context import MSAContext
 from chai_lab.data.parsing.msas.aligned_pqt import parse_aligned_pqt_to_msa_context
 
 
@@ -13,10 +14,9 @@ def test_monomer():
     sequence = "GPKAMAETRECIYYNANWELERTNQSGLERCEGEQDKRLHCYASWRNSSGTIELVKKGCWLDDFNCYDRQECVATEENPQVYFCCCEGNFCNERFTHLP"
 
     with TemporaryDirectory() as tmpdir:
-        tmpdirpath = Path(tmpdir)
         seq_to_aligned_pqt = generate_colabfold_msas(
             [sequence],
-            msa_dir=tmpdirpath,
+            msa_dir=Path(tmpdir),
             msa_server_url="https://api.colabfold.com",
             search_templates=False,
         )
@@ -25,7 +25,28 @@ def test_monomer():
 
         (pqt_file,) = seq_to_aligned_pqt.values()
         assert pqt_file.exists()
-        ctx = parse_aligned_pqt_to_msa_context(pqt_file)
+        ctx: MSAContext = parse_aligned_pqt_to_msa_context(pqt_file)
 
         # First row of the context should correspond to
         assert ctx.ith_sequence(0) == sequence
+
+
+def test_multimer():
+    sequences = [
+        "GPKAMAETRECIYYNANWELERTNQSGLERCEGEQDKRLHCYASWRNSSGTIELVKKGCWLDDFNCYDRQECVATEENPQVYFCCCEGNFCNERFTHLP",
+        "QVQLVQSGAEVKKPGASVKVSCKASGYTFTSSYINWVRQAPGQGLEWMGTINPVSGSTSYAQKFQGRVTMTRDTSISTAYMELSRLRSDDTAVYYCARGGWFDYWGQGTLVTVSSHHHHHH",
+    ]
+
+    with TemporaryDirectory() as tmpdir:
+        seq_to_aligned_pqt = generate_colabfold_msas(
+            sequences,
+            msa_dir=Path(tmpdir),
+            msa_server_url="https://api.colabfold.com",
+            search_templates=False,
+        )
+        assert len(seq_to_aligned_pqt) == 2
+        for seq in sequences:
+            pqt_file = seq_to_aligned_pqt[seq]
+            ctx: MSAContext = parse_aligned_pqt_to_msa_context(pqt_file)
+            assert ctx.depth > 1
+            assert ctx.ith_sequence(0) == seq

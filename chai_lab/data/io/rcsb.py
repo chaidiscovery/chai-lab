eb63e84536bcbb46c0b675abb6a312ec9d682443
Kevin Wu <kevin@chaidiscovery.com>
2025-05-01 08:52:00 -0700

    Global template cache folder + envvar to control its location (#371)
    * Add docstring
    
    * Global template cif cache folder
    
    * Update readme

--- chai_lab/data/io/rcsb.py
+++ chai_lab/data/io/rcsb.py
@@ -7,7 +7,10 @@ from chai_lab.utils.paths import download_if_not_exists
 
 
 def download_cif_file(pdb_id: str, directory: Path) -> Path:
-    """Download the cif file for the given PDB ID from RCSB into the directory."""
+    """Download the cif file for the given PDB ID from RCSB into the directory.
+
+    No-op if the directory/pdb_id.cif.gz already exists.
+    """
     outfile = directory / f"{pdb_id}.cif.gz"
     source_url = f"https://files.rcsb.org/download/{pdb_id}.cif.gz"
     download_if_not_exists(source_url, outfile)

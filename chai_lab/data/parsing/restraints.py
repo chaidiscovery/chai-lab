f0278acc055f6eb8519b2767285c8a3b9dcff3f2
Alex Rogozhnikov <iamfullofspam@gmail.com>
2025-09-05 18:35:57 +0000

    update pandera

--- chai_lab/data/parsing/restraints.py
+++ chai_lab/data/parsing/restraints.py
@@ -11,7 +11,7 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
-import pandera as pa
+import pandera.pandas as pa
 from pandera.typing import Series
 from typing_extensions import assert_never
 
